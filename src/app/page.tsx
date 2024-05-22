e626e1e597ea2e11c69992ba48c4f95b91686ba1
Elad Karni <elad.karni@gmail.com>
2024-05-22 08:09:06 -0400

    Fix: Make sure content doesn't touch window edges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import FAQSection from "@/views/faq";
 
 export default function Home() {
   return (
-    <main className="text-center max-w-[1100px] mx-4 md:mx-auto">
+    <main className="text-center max-w-[1100px] mx-4 xl:mx-auto">
       <h1>Tailwind + NextJS Accodion Example </h1>
       <FAQSection faqItems={faqItems} />
     </main>
